0c947ac2b86ea5eb5160e5a044b7c62b30586895
Praveen Soni <praveensoni9291@gmail.com>
2023-06-10 13:30:49 +0530

    assignment2

---
+++ script.js
@@ -0,0 +1,9 @@
+var heading= document.getElementById('main-header');
+heading.style.borderBottom= 'solid 3px black';
+
+var main = document.getElementById('main');
+
+main.firstElementChild.style.color='green';
+main.firstElementChild.style.fontWeight= 'bold';
+
+
